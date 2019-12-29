b2eceb33549d57fcdc502409b188ed3edd76e385
lk2322 <lk2322@ya.ru>
2019-12-30 00:03:51 +0500

    Защита от ошибок

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ en_ru = {
 
 
 }
-line = list(input().lower())
-res = []
-for i in line:
-    res.append(en_ru.get(i, ''))
-print(''.join(res).capitalize())
-input()
+ru_en = {'й': 'q', 'ц': 'w', 'у': 'e', 'к': 'r', 'е': 't', 'н': 'y', 'г': 'u', 'ш': 'i', 'щ': 'o', 'з': 'p', 'х': '[{', 'ъ': ']}', 'ф': 'a',
+         'ы': 's', 'в': 'd', 'а': 'f', 'п': 'g', 'р': 'h', 'о': 'j', 'л': 'k', 'д': 'l',
+         'ж': ';:', 'э': '\'"', 'я': 'z', 'ч': 'x', 'с': 'c', 'м': 'v',
+         'и': 'b', 'т': 'n', 'ь': 'm', 'б': ',<', 'ю': '.>', ' ': ' ', '.': '/', ',': '?', '?': '&'}
+
+try:
+    line = list(input().lower())
+    res = []
+    for i in line:
+        res.append(en_ru.get(i, ''))
+    print(''.join(res).capitalize())
+    input()
+except Exception:
+    print('Упс... Произошла ошибка')
